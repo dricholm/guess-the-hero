7bab90ac54a0513f8d1cb9ce3cf50e2705848e70
David Richolm <32329112+dricholm@users.noreply.github.com>
2023-03-18 06:13:20 -0700

    fix: Improve play UX (#42)
    - Adjust layout for mid-screens.
    - Scroll to top on new game.

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -1,13 +1,15 @@
+import clsx from 'clsx';
 import { FC, ReactNode } from 'react';
 import styles from './Card.module.scss';
 
 interface CardProps {
   children: ReactNode;
+  className?: string;
   title?: string;
 }
 
-const Card: FC<CardProps> = ({ children, title }) => (
-  <div className={styles.container}>
+const Card: FC<CardProps> = ({ children, className, title }) => (
+  <div className={clsx(styles.container, className)}>
     {title && <h2 className={styles.title}>{title}</h2>}
     <div className={styles.body}>{children}</div>
   </div>
